b79d072f9f985283119bcd7fa0b139bbb60657d9
Phuc <tranthienphuck81@gmail.com>
2026-04-16 14:54:54 +0700

    new update

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,13 @@
-extends Node2D
+extends CharacterBody2D
 
 
 var direction: Vector2 = Vector2(1,1)
-var speed: int = 5
+var speed: int = 200
 
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("Left","Right","Up","Down")	
-	position += direction * speed
+	velocity = direction * speed
+	move_and_slide()
 	
 	if Input.is_action_just_pressed("Confirm"):
 		print("Chi chay 1 lan khi bam Space")
