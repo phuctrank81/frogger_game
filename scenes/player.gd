8e658e1d73f99fca513162834b40d376901388d3
Phuc <tranthienphuck81@gmail.com>
2026-05-17 16:22:36 +0700

    New update

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,7 +7,19 @@ var speed: int = 200
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("Left","Right","Up","Down")	
 	velocity = direction * speed
+	animation()
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("Confirm"):
 		print("Chi chay 1 lan khi bam Space")
+	
+func animation():
+	if direction:
+		$AnimatedSprite2D.flip_h = direction.x > 0
+		if direction.x != 0:
+			$AnimatedSprite2D.animation = 'left'
+		else:
+			$AnimatedSprite2D.animation = 'up' if direction.y < 0 else 'down'
+	else:
+		$AnimatedSprite2D.frame = 0
+	
