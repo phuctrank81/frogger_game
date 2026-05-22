df2a918b954d0c3ae07046c4aff23f81df2daec8
Phuc <minecraft811500@gmail.com>
2026-05-22 10:53:25 +0700

    new thing change

--- scenes/car.gd
+++ scenes/car.gd
@@ -4,11 +4,17 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 var direction = Vector2.LEFT
 var speed = 100
+var colors = [
+	preload("res://graphics/cars/green.png"),
+	preload("res://graphics/cars/red.png"),
+	preload("res://graphics/cars/yellow.png")
+	]
 
 func _ready() -> void:
 	if position.x < 0:
 		direction.x = 1
 		$Sprite2D.flip_h = true
+	$Sprite2D.texture = colors.pick_random()
 
 func _process(delta: float) -> void:
 	position += direction * speed * delta
