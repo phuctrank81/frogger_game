58f45d6aa5216c21aaf318735b34699d019ccd62
Phuc <minecraft811500@gmail.com>
2026-05-23 10:34:21 +0700

    new update

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,9 +3,12 @@ extends Node2D
 var car_scene: PackedScene = preload("res://scenes/car.tscn")
 var score: int 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print(body)
-	print("Player have entered")
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	call_deferred("change_scene")
+	
+
+func change_scene():
+	get_tree().change_scene_to_file("res://scenes/Title.tscn")
 
 
 func _on_car_timer_timeout() -> void:
@@ -15,13 +18,12 @@ func _on_car_timer_timeout() -> void:
 	$Objects.add_child(car)
 	car.connect("body_entered", go_to_title)
 
-func go_to_title(body):
-	print(body)
-	print('Player car collision')
-	
+func go_to_title(_body):
+	call_deferred("change_scene")
+	if score < Global.score:
+		Global.score = score
 
 
 func _on_score_timer_timeout() -> void:
 	score += 1
 	$CanvasLayer/Label.text = 'Time elapsed: ' + str(score)
-	
